598d71d032a324616ab71c308cea22eb43c1ab1c
Andrey Ivanov <aivanov@thumbtack.net>
2013-03-26 02:59:15 +0700

    added block with last tests

--- web/app/controllers/IndexController.php
+++ web/app/controllers/IndexController.php
@@ -6,11 +6,19 @@ class IndexController extends ControllerBase
     public function indexAction()
     {
         $this->view->setVar("title", "NoSQL Test Results Repository");
-        $tags = array();
-        foreach(Tags::find() as $tag){
-            $tags[] = $tag->name;
-        }
+
+        $tags_collection = Tags::findFirst();
+        $tags = $tags_collection->tags;
+        $prefixes = $tags_collection->prefixes;
         $this->view->setVar("tags", $tags);
+        $this->view->setVar("tags_prefixes", $prefixes);
+
+        $results = array();
+        $results_resourse = Results::find(array("limit" => 10, "sort" => array("datetime" => -1)));
+        foreach($results_resourse as $result){
+            $results[] = $result;
+        }
+        $this->view->setVar("results", $results);
     }
 
 }
