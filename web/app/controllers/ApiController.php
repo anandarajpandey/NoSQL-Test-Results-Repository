ecc91b20513c03314db1d2a3d2258ff1a0056087
Andrey Ivanov <aivanov@thumbtack.net>
2013-04-16 19:39:57 +0700

    added search of tests

--- web/app/controllers/ApiController.php
+++ web/app/controllers/ApiController.php
@@ -12,8 +12,20 @@ class ApiController extends ControllerBase
         exit();
     }
 
-    public function getResults(){
-
+    public function getResultsAction(){
+        $formatted_results = array();
+        $results = Results::find(array("conditions" => array("tags" => array('$all' => $_GET['tags']))));
+        foreach($results as $result){
+            $formatted_results[] = array(
+                "id" => (string)$result->_id,
+                "name" => $result->name,
+                "throughput" => $result->result['throughput'],
+                "read" => array("o" => $result->result['read']['ops'], "s" => $result->result['read']['successes']),
+                "write" => array("o" => $result->result['write']['ops'], "s" => $result->result['write']['successes']),
+            );
+        }
+        echo json_encode($formatted_results);
+        exit();
     }
 
 }
--- web/public/js/initial.js
+++ web/public/js/initial.js
@@ -1,38 +1,70 @@
 $(function(){
-    $("#tags").chosen({});
-    $(".search_test_button").on("click", function(){
+    //todo refactor this file
+    $(".tags").chosen({});
+
+    var selected_tests = [];
+    function rows_hadler(){
+        $(".test_row").on("click", function(e){
+            var that = $(this).toggleClass("selected");
+            var test = that.find(".test");
+            if(!$(e.target).hasClass("test")){
+                test.prop("checked", !test.is(":checked"));
+            }
+            var index = selected_tests.indexOf(test.attr("id"));
+            if (index !== -1){
+                selected_tests.splice(index, 1);
+            } else {
+                selected_tests.push(test.attr("id"));
+            }
+        });
+    }
+    rows_hadler();
+
+    $(".search_tests_button").on("click", function(){
         $.ajax({
             type    : "GET",
             url     : "/api/getResults",
-            data    : $("#tags").val()
+            data    : {'tags': $("#search .tags").val()},
+            'dataType': 'json',
+            success : function(response){
+                draw_results_table(response, $("#search_results"));
+            },
+            error   : function(e, mesage){
+                Log.add(mesage);
+            }
         });
     });
-    var selected_tests = [];
-    $(".test_row").on("click", function(e){
-        //todo refactor this handler
-        var that = $(this).toggleClass("selected");
-        var test = that.find(".test");
-        if(!$(e.target).hasClass("test")){
-            test.prop("checked", !test.is(":checked"));
+
+    function draw_results_table(results, $container){
+        $(".test_row").remove();
+        for (var key in results){
+            var result = results[key];
+            $row =  $(["<tr class='test_row'>",
+                            "<td><input type='checkbox' class='test' id='", result.id, "'></td>",
+                            "<td>", result.name, "</td>",
+                            "<td>", result.throughput, "</td>",
+                            "<td>", result.read.o, "</td>",
+                            "<td>", result.read.s, "</td>",
+                            "<td>", result.write.o, "</td>",
+                            "<td>", result.write.s, "</td>",
+                      "</tr>"].join(''));
+            $container.prepend($row);
         }
-        var index = selected_tests.indexOf(test.attr("id"));
-        if (index !== -1){
-            selected_tests.splice(index, 1);
-        } else {
-            selected_tests.push(test.attr("id"));
-        }
-    });
+        rows_hadler();
+    }
+
     $(".draw_chart_button").on("click", function(){
-        var mock = {
-            'tests' : selected_tests,
-            'axes' : ['throughput'],
-            'group' : ['DataSetSize-6GB']
-        };
-        var chart = new Chart(mock, $("#chart"));
+
         if(selected_tests.length > 0){
+            var mock = {
+                'tests' : selected_tests,
+                'axes' : ['throughput'],
+                'group' : ['DataSetSize-6GB']
+            };
+            var chart = new Chart(mock, $("#chart"));
             chart.draw();
         }else{
-            chart.setError('Please, select a few tests results');
+            $("#chart").html('Please, select a few tests results');
         }
 
     })
