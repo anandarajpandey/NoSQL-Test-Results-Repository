2a1ffa08b28eb8013fa4fa2fa1891b232954941c
zdrul <zdrul@zdrul-pc.Dlink>
2013-04-17 03:17:28 +0700

    refactoring of search tests results

--- web/public/js/initial.js
+++ web/public/js/initial.js
@@ -1,60 +1,16 @@
 $(function(){
-    //todo refactor this file
-    $(".tags").chosen({});
 
-    var selected_tests = [];
-    function rows_hadler(){
-        $(".test_row").on("click", function(e){
-            var that = $(this).toggleClass("selected");
-            var test = that.find(".test");
-            if(!$(e.target).hasClass("test")){
-                test.prop("checked", !test.is(":checked"));
-            }
-            var index = selected_tests.indexOf(test.attr("id"));
-            if (index !== -1){
-                selected_tests.splice(index, 1);
-            } else {
-                selected_tests.push(test.attr("id"));
-            }
-        });
-    }
-    rows_hadler();
+    $(".tags").chosen({});
+    $(".axe").chosen({});
+
+    var res = new results($(".tests_table"));
 
     $(".search_tests_button").on("click", function(){
-        $.ajax({
-            type    : "GET",
-            url     : "/api/getResults",
-            data    : {'tags': $("#search .tags").val()},
-            'dataType': 'json',
-            success : function(response){
-                draw_results_table(response, $("#search_results"));
-            },
-            error   : function(e, mesage){
-                Log.add(mesage);
-            }
-        });
+        res.searchByTags($("#search .tags").val());
     });
 
-    function draw_results_table(results, $container){
-        $(".test_row").remove();
-        for (var key in results){
-            var result = results[key];
-            $row =  $(["<tr class='test_row'>",
-                            "<td><input type='checkbox' class='test' id='", result.id, "'></td>",
-                            "<td>", result.name, "</td>",
-                            "<td>", result.throughput, "</td>",
-                            "<td>", result.read.o, "</td>",
-                            "<td>", result.read.s, "</td>",
-                            "<td>", result.write.o, "</td>",
-                            "<td>", result.write.s, "</td>",
-                      "</tr>"].join(''));
-            $container.prepend($row);
-        }
-        rows_hadler();
-    }
-
     $(".draw_chart_button").on("click", function(){
-
+        var selected_tests = res.getSelectedResults();
         if(selected_tests.length > 0){
             var mock = {
                 'tests' : selected_tests,
---
+++ web/public/js/results.js
@@ -0,0 +1,81 @@
+var results = function($container){
+
+    var _$container = $container;
+
+    var $results_container = _$container.find(".results");
+
+    var selected_results = [];
+
+    var row_handler = function(e){
+        var $this = $(this);
+        $this.toggleClass("selected");
+        var test = $this.find(".test");
+        if(!$(e.target).hasClass("test")){
+            test.prop("checked", !test.is(":checked"));
+        }
+        var index = selected_results.indexOf(test.attr("id"));
+        if (index !== -1){
+            selected_results.splice(index, 1);
+        } else {
+            selected_results.push(test.attr("id"));
+        }
+    };
+
+    var setData = function(data){
+        $results_container.empty();
+        selected_results = [];
+        for (var key in data){
+            var result = data[key];
+            var $row =  $(["<tr class='test_row'>",
+                "<td><input type='checkbox' class='test' id='", result.id, "'></td>",
+                "<td>", result.name, "</td>",
+                "<td>", result.throughput, "</td>",
+                "<td>", result.read.o, "</td>",
+                "<td>", result.read.s, "</td>",
+                "<td>", result.write.o, "</td>",
+                "<td>", result.write.s, "</td>",
+                "</tr>"].join('')).on("click", row_handler);
+            $results_container.append($row);
+        }
+    };
+
+    $container.find(".test_row").on("click", row_handler);
+
+    var writeMessage = function(message, error){
+        if(error){
+            error = 'error';
+        }else{
+            error = '';
+        }
+        $results_container.html("<tr><td colspan='7' class='tip_in_table "+error+"'>"+message+"</td></tr>")
+    }
+
+
+
+    return {
+        'getSelectedResults' : function(){
+            return selected_results;
+        },
+        'searchByTags' : function(tags){
+            Log.add(tags);
+            if(tags && tags.length > 0){
+                writeMessage("Loading...");
+                $.ajax({
+                    type    : "GET",
+                    url     : "/api/getResults",
+                    data    : {'tags': tags},
+                    'dataType': 'json',
+                    success : function(response){
+                        setData(response);
+                    },
+                    error   : function(e, mesage){
+                        Log.add(mesage);
+                    }
+                });
+            }else{
+                writeMessage("Select one or more tags for search", 1);
+            }
+
+        }
+    }
+};
